3f464b8870ae3520508a6835cf2306d02639cbce
Jerod Estapa <jerodestapa@gmail.com>
2015-10-02 16:05:53 -0500

    Added comments

--- controllers.js
+++ controllers.js
@@ -1,5 +1,6 @@
 // CONTROLLERS
 
+// HOME CONTROLLER
 weatherApp.controller('homeController', ['$scope', '$location', 'cityService',  function($scope, $location, cityService) {
     
     $scope.city = cityService.city;
@@ -16,12 +17,14 @@ weatherApp.controller('homeController', ['$scope', '$location', 'cityService',  
     
 }]);
 
+// FORECAST CONTROLLER
 weatherApp.controller('forecastController', ['$scope', '$routeParams', 'cityService', 'weatherService', function($scope, $routeParams, cityService, weatherService) {
 
     $scope.city = cityService.city;
     
     $scope.days = $routeParams.days || 2;
     
+    // Connects weather API in services.js to this controller
     $scope.weatherResult = weatherService.GetWeather($scope.city, $scope.days);
     
     $scope.convertToFahr = function(degK) {
