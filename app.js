3369582129ff4eb5dfb9b7bfb3738f6197b1fbae
jerodestapa <jerodestapa@gmail.com>
2015-10-01 21:41:51 -0500

    Added directives folder

--- app.js
+++ app.js
@@ -18,6 +18,11 @@ weatherApp.config(function ($routeProvider) {
         controller: 'forecastController'
     })
     
+    .when('/forecast/:days', {
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    })
+    
 });
 
 // SERVICES
@@ -40,15 +45,46 @@ weatherApp.controller('homeController', ['$scope', 'cityService',  function($sco
     
 }]);
 
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
 
     $scope.city = cityService.city;
     
-    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?q=London,us&cnt=2", 
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?q=London", 
     { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
     
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2 });
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
     
-    console.log($scope.weatherResult);
+    $scope.convertToFahr = function(degK) {
+        
+        return Math.round((1.8 * (degK - 273))+ 32);
+                          
+    };
+    
+    $scope.convertToDate = function(dt) {
+    
+        return new Date(dt * 1000);
+        
+    };
     
 }]);
+
+// DIRECTIVES
+
+weatherApp.directive('weatherReport', function() {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'directives/weatherReport.html',
+        replace: true,
+        scope: {
+            weatherDay: "=",
+            convertToStandard: "&",
+            convertToDate: "&",
+            dateFormat: "@"
+        }
+    
+    }
+
+};
